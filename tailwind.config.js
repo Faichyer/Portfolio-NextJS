a736e533583f8e17b63829dcaa48fb4bd698e043
Miki Namsrai <miki.namsrai@gmail.com>
2021-02-05 20:12:31 +0100

    add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#1e1e1e',
+        noir: '#111111'
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem'
+      },
+      fontFamily: {
+        'title': ['Montserrat', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+        'barcode' : ['Libre Barcode 39', 'cursive']
+      }
+    },
   },
   variants: {
     extend: {},
